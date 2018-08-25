c3ef1b4841aa5fdec523c6777866db7d8481e298
root <dasgupta.sushanta@gmail.com>
2018-08-25 22:36:14 +0530

    new libgdx project

---
+++ desktop/src/com/bravo/johny/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.bravo.johny.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.bravo.johny.game.GdxSamplerGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GdxSamplerGame(), config);
+	}
+}
